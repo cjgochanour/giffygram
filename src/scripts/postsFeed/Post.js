efe22e5eeadf0ba144c7d48c9ffbc657a4a77d8b
Sara Hackle <Shackle@ivytech.edu>
2022-01-05 15:57:29 -0600

    created html for individual posts

---
+++ src/scripts/postsFeed/Post.js
@@ -0,0 +1,16 @@
+import {getUsers} from "./dataAccess.js";
+//function returns post so that it can appear in post list
+export const Post = (post) => {
+  const users = getUsers();
+  const author = users.find(user=> user.id === post.authorId);
+  
+    return `
+        <li class ="post postListItem">
+            <h2 class="postItem postSubject">${post.subject}<h2>
+            <img class="postItem postImage" src="${post.gifLink}" width="200px"/>
+            <div class= "postItem postStory">${post.story}</div>
+            <div class="postItem postAuthor">Posted by: ${author.firstName} ${author.lastName}</div>
+            <div class="postItem postDate">${new Date(post.timestamp).toLocaleDateString("en-US")} </div>    
+        
+        `;
+};

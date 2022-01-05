5fe155856dc66e4fe0c903ad995df52c498dadfa
cjgochanour <cjgochanour@gmail.com>
2022-01-05 14:42:22 -0600

    removed fieldset, added x img and button

--- src/scripts/createForms/PostCreate.js
+++ src/scripts/createForms/PostCreate.js
@@ -1,14 +1,12 @@
 export const PostCreate = () => {
     return `
         <div class="PostCreate">
+            <img class="closeBtn" src="images/closeX.png" alt="close post creation">
             <form>
-                <fieldset>
-                    <legend>Create A Post</legend>
-
-                    <input type="text" name="gifURL" id="postUrlInput" placeholder="URL:" />
-                    <input type="text" name="postTitle" id="postTitle" placeholder="Title:" />
-                    <textarea name="postComment" id="postComment" placeholder="Comment:" />
-                </fieldset>
+                <input type="text" name="gifURL" id="postUrlInput" placeholder="URL:" />
+                <input type="text" name="postTitle" id="postTitle" placeholder="Title:" />
+                <textarea name="postComment" id="postComment" placeholder="Comment:" />
             </form>
+            <button id="sendPost">Submit Post</button>
         </div>`;
 };

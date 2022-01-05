31a4d14b6a4be906b420b5fb7d8fe8b2696d0c5c
Sara Hackle <Shackle@ivytech.edu>
2022-01-05 13:47:51 -0600

    Create Message HTML

---
+++ src/scripts/createForms/MessageCreate.js
@@ -0,0 +1,28 @@
+import { getUsers } from "./dataAccess.js";
+
+export const messageForm = () => {
+  const users = getUsers();
+
+  let html = `        
+    <div class="messageForm">
+        <form>
+            <fieldset>
+                <select class="messageFormItem recipientList">
+                    <option value = "0">Choose Recipient</option>
+                        ${users.map((user) => {
+                            return `
+                                <option class="recipientListItem" value="${user.id}">${user.firstName} ${user.lastName}</option>
+                            `;
+                        })
+                        .join("")}
+                </select>
+            </fieldset>
+            <fieldset>
+                <label class="messageFormItem msgTextLabel" for="messageCreate">Message</label>
+                    <textarea type="text" name="messageCreate"></textarea>
+            </fieldset>
+        </form>
+            <button id="sendMessage">Send</button>
+    </div>`;
+    return html;
+};

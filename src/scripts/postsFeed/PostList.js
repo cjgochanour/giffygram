c89d1467a7cf0e9a24f28c830385b5340b519120
cjgochanour <cjgochanour@gmail.com>
2022-01-05 15:35:44 -0600

    added post filter and PostList html

---
+++ src/scripts/postsFeed/PostList.js
@@ -0,0 +1,17 @@
+import { getChosenUser } from "../dataAccess.js";
+import { Post } from "./Post.js";
+import { getPosts } from "../dataAccess.js";
+
+export const PostList = () => {
+    let posts = getPosts();
+    const chosenUser = getChosenUser();
+    // if chosenUser = null, display all posts
+    // if chosenUser = x, display posts from x
+    if (chosenUser) {
+        posts = posts.filter((post) => post.authorId === chosenUser);
+    }
+
+    return `<ul><li><button class="postListItem postBtn">Create A Post</button></li>${posts
+        .map((post) => Post(post))
+        .join("")}</ul>`;
+};

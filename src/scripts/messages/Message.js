30a995087630344f52e7da83d4cd5592c457ddaf
Seracrooce <fschmidt@live.com>
2022-01-06 15:05:29 -0600

    notifications will show up on navbar. Beginning of adding message read functionalily

--- src/scripts/messages/Message.js
+++ src/scripts/messages/Message.js
@@ -1,4 +1,4 @@
-import { getUsers } from "../dataAccess.js";
+import { getUsers, updateMessage, getMessages } from "../dataAccess.js";
 
 //function that returns the html for a message to appear in the message list
 export const Message = (msg) => {
@@ -13,3 +13,20 @@ export const Message = (msg) => {
             <img class="msgItem msgMarkRead" id="msgRead--${msg.id}" src="./images/betaLogo.png" alt="Mark As Read" width="20px"/>
         </li>`
 } 
+
+//grab container
+mainContainer = document.querySelector(".beta");
+
+//event listeners for pressing read button
+mainContainer.addEventListener("click", clickEvent => {
+    if (clickEvent.target.id.startsWith("msgRead")) {
+        const [ ,msgIdString] = clickEvent.target.id.split("--");
+        const msgId = parseInt(msgIdString);
+        const messages = getMessages();
+        const currentMessage = messages.find(msg => msg.id === msgId);
+        currentMessage.read = true;
+        
+        updateMessage(currentMessage)
+            .then(() => fetchMessages())
+    }
+})
--- src/scripts/navBar/NavBar.js
+++ src/scripts/navBar/NavBar.js
@@ -1,26 +1,29 @@
-import { getCurrentUser, getLikes, getUsers } from "../dataAccess.js"
+import { getCurrentUser, getMessages, getUsers } from "../dataAccess.js"
 import { msgClose, msgOpen, getMsgCol, setMsgColFalse, setMsgColTrue } from "../messages/MessageSideBar.js";
-import {LoginForm} from "../auth/Login.js"
 
 export const Navbar = () => {
     const currentUser = getCurrentUser();
     const users = getUsers();
+    const allMessages = getMessages();
+    const userMessages = allMessages.filter(msg => msg.recipientId === currentUser.id);
+
     // we'll add specific images later
     return `
         <img class="navbarItem navColBtn" id="navColBtn" src="images/betaLogo.png" alt="AltText" width="50px" />
         <section class="navbarCollapse navbarItem">
-            <span id="homeBtn" class="navcolItem"><img src="images/betaLogo.png" alt="AltText" width="50px" />Home</span>
-            <span id="userBtn" class="navcolItem"><img src="images/betaLogo.png" alt="AltText" width="50px" />${currentUser.firstName} ${currentUser.lastName}</span>
-            <span id="notification" class="navcolItem notification"><img id="notifImg" class="notification notifImg" src="images/betaLogo.png" alt="AltText" width="50px" />Notifications</span>
-            <span id="writeMessageBtn" class="navcolItem"><img src="images/betaLogo.png" alt="AltText" width="50px" />Compose Message</span>
-            <span id="postGifBtn" class="navcolItem"><img src="images/betaLogo.png" alt="AltText" width="50px" />Post Gif</span>
+            <div id="homeBtn" class="navcolItem"><img src="images/betaLogo.png" alt="AltText" width="50px" />Home</div>
+            <div id="userBtn" class="navcolItem"><img src="images/betaLogo.png" alt="AltText" width="50px" />${currentUser.firstName} ${currentUser.lastName}</div>
+            <div id="notification" class="navcolItem notification"><img id="notifImg" class="notification notifImg" src="${(userMessages.length > 0) ? "images/notification-bell-filled.png" : "images/notification-bell-empty.png"}" alt="AltText" width="30px" />${(userMessages.length > 0) ? `<div class="notifNumberContainer"><span class="notifNumber">${userMessages.length}</span></div>` : ""}</div>
+            <div id="writeMessageBtn" class="navcolItem"><img src="images/betaLogo.png" alt="AltText" width="50px" />Compose Message</div>
+            <div id="postGifBtn" class="navcolItem"><img src="images/betaLogo.png" alt="AltText" width="50px" />Post Gif</div>
+            <div id="filterLikeBtn" class="navcolItem"><img src="images/betaLogo.png" alt="AltText" width="50px" />Filter By Likes</div>
             <select id="filterDropDown" class="navcolItem">
                     <option value="0">Filter by name</option>
                     ${users.map(user => {
                         return `<option value="${user.id}">${user.firstName} ${user.lastName}</option>`
                     }).join("")}
             </select>
-            <span id="logoutBtn" class="navcolItem"><img id="logoutImg" src="images/betaLogo.png" alt="AltText" width="50px" />Log Out</span>
+            <div id="logoutBtn" class="navcolItem"><img id="logoutImg" src="images/betaLogo.png" alt="AltText" width="50px" />Log Out</div>
         </section>
     `
 }
